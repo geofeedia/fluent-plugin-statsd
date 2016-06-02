8b0ecae88bb95ee2a0e375827225d9d9d3cbe2d1
Charlie Moad <charlie.moad@geofeedia.com>
2016-06-02 16:32:22 -0400

    initial port to our custom metric needs

--- lib/fluent/plugin/out_statsd.rb
+++ lib/fluent/plugin/out_statsd.rb
@@ -5,7 +5,7 @@ module Fluent
     Fluent::Plugin.register_output('statsd', self)
 
     config_param :flush_interval, :time, :default => 1
-    config_param :host, :string, :default => 'localhost'
+    config_param :host, :string, :default => '127.0.0.1'
     config_param :port, :string, :default => '8125'
 
     attr_reader :statsd
@@ -28,28 +28,46 @@ module Fluent
     end
 
     def format(tag, time, record)
-      record.to_msgpack
+      [tag, time, record].to_msgpack
     end
 
     def write(chunk)
-      chunk.msgpack_each {|record|
-        if statsd_type = record['statsd_type']
-          case statsd_type
-          when 'timing'
-            @statsd.timing record['statsd_key'], record['statsd_timing'].to_f
-          when 'gauge'
-            @statsd.gauge record['statsd_key'], record['statsd_gauge'].to_f
-          when 'count'
-            @statsd.count record['statsd_key'], record['statsd_count'].to_f
-          when 'set'
-            @statsd.set record['statsd_key'], record['statsd_set']
-          when 'increment'
-            @statsd.increment record['statsd_key']
-          when 'decrement'
-            @statsd.decrement record['statsd_key']
+      chunk.msgpack_each do |tag, time, record|
+        level = tag.split('.').last.downcase
+        if level != info
+          next # all our metrics are logged at an info level
+        end
+
+        # prep the placement tags
+        tags = []
+        %w(cloud hostname instanceid podname region zone).each do |tag|
+          if record.has_key? "placement.#{tag}"
+            tags << tag + '=' + record['placement.' + tag]
           end
         end
-      }
+        tags = tags.join(',')
+        if not tags.empty?
+          tags = ',' + tags # prefix a comma for below
+        end
+
+        record.each_key do |field|
+
+          # timer
+          if field.match /took<(long|int)>$/
+            @statsd.timing field.sub /<(long|int)>$/, '' + tags, record[field].to_i
+
+          # counters
+          elsif field.match /count<(long|int)>$/
+            @statsd.increment field.sub /<(long|int)>$/, '' + tags, record[field].to_i
+          elsif field.match /success<int>$/
+            @statsd.increment field.sub /<int>$/, '' + tags, record[field].to_i
+          elsif field.match /error<int>$/
+            @statsd.increment field.sub /<int>$/, '' + tags, record[field].to_i
+          
+          end
+        end
+
+      end
     end
 
   end
